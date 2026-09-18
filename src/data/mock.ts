043c9e4a31ab9d0789392ce5fd149dbaef8a171c
sevq-app <sevqapp@yandex.ru>
2026-09-18 16:41:22 +0300

    добавили каллс

--- src/data/mock.ts
+++ src/data/mock.ts
@@ -4,10 +4,11 @@ export type Screen =
   | 'chats'
   | 'conversation'
   | 'friends'
-  | 'clubs'
+  | 'calls'       // <-- ДОБАВЛЕНО
+  | 'clubs'       // (можно оставить на всякий случай, или удалить)
   | 'profile'
   | 'search'
-  | 'settings'; // <-- ДОБАВЛЕНО!
+  | 'settings';
 
 export interface Message {
   id: string;
